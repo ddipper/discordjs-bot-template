1c4699bb624949e9da1479569a15a247ca334554
ddipperq <dimonbyt@gmail.com>
2024-02-21 21:25:38 +0300

    update events initialize

--- src/utils/events_init.js
+++ src/utils/events_init.js
@@ -1,10 +1,11 @@
 const fs = require('node:fs')
-const path = __dirname + '/../../events/';
+const path = require('node:path')
+const events_path = __dirname + './../events/';
 
 module.exports = (discord) => {
-   const eventFiles = fs.readdirSync(path).filter(file => file.endsWith('.js'));
+   const eventFiles = fs.readdirSync(events_path).filter(file => file.endsWith('.js'));
    for (const file of eventFiles) {
-      const filePath = path.join(path, file);
+      const filePath = path.join(events_path, file);
       const event = require(filePath);
       if (event.once) {
          discord.once(event.name, (...args) => event.execute(...args));
